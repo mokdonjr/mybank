c4f303ef11de1af6f1bc170584d527590b0105d6
baegseungchan-gram <mokdonjr@gmail.com>
2020-03-03 00:56:02 +0900

    spring initializr

---
+++ src/main/java/com/example/mybankserver/MybankServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybankserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybankServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybankServerApplication.class, args);
+	}
+
+}
